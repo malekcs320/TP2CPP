6eab0566f83358213808686a89055e189c9b1c6f
malekcs320 <malek.mahjoub1999@gmail.com>
2021-11-19 16:12:38 +0100

    premier commit

---
+++ Trajet.h
@@ -0,0 +1,75 @@
+/*************************************************************************
+                           Xxx  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Xxx> (fichier Xxx.h) ----------------
+#if ! defined ( TRAJET_H )
+#define TRAJET_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Xxx>
+//
+//
+//------------------------------------------------------------------------
+
+class Trajet :
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Trajet ( const Xxx & unXxx );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+char* depart;
+char* arrivee;
+
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Xxx>
+
+#endif // XXX_H
+
